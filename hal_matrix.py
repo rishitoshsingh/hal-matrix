4a52c6e931b6340d21079b4c25de6c935aeffb58
RIshitosh Singh <rishitoshs@gmail.com>
2020-11-30 23:56:16 +0530

    initial datastructure

--- hal_matrix.py
+++ hal_matrix.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import bsr_matrix
+from scipy.sparse import dia_matrix, bmat
 
 class Hal_Matrix:
     def __init__(self, n, d, data):
@@ -14,11 +14,15 @@ class Hal_Matrix:
         self.d=d
         assert data.shape==(n**2,d), 'data is of incorrect shape, it shpuld be ({},{})'.format(n**2,d)
         self.data=data
-        self.shape = (d*n, d*n)
-    
-    
-    
 
+        dia_matrices = []
+        for dia_data in data:
+            dia_matrices.append(dia_matrix((dia_data,[0]),shape=(d,d)))
+        dia_matrices = np.array(dia_matrices).reshape(n,n)
+        self.matrix=bmat(dia_matrices)
+        self.shape = self.matrix.shape
+
+    
 data=np.array([[1,2,3,4]]).repeat(4,axis=0)
 x = Hal_Matrix(n=2,d=4, data=data)
 print(x.shape)
